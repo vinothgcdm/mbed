6892d8e7c4a73f80245c7dc93e7fed3e64c282e7
Vinoth Gnanasekaran <vinothg.cdm@gmail.com>
2016-12-26 13:23:52 +0530

    Send data to emoncms server using ESP8266Interface

--- esp8266/main.cpp
+++ esp8266/main.cpp
@@ -1,118 +1,67 @@
-// #include "mbed.h"
-// #include "esp8266-driver/ESP8266/ATParser/ATParser.h"
+#include "mbed.h"
+#include "TCPSocket.h"
+#include "esp8266-driver/ESP8266Interface.h"
 
-// Serial pc(USBTX, USBRX);
-// BufferedSerial wifi(P2_8, P2_9, 1024);
-// ATParser parser(wifi);
+#define KEY1 P2_6
+#define KEY2 P2_7
 
-// void wifi_init(void)
-// {
-//    DigitalOut boot_wifi(P0_22);
-//    DigitalOut rest_wifi(P1_19);
+void wifi_init(void)
+{
+    DigitalOut boot_wifi(P0_22);
+    DigitalOut rest_wifi(P1_19);
 
-//    wait_us(500); // Approximate time value
-//    boot_wifi = 0;
-//    rest_wifi = 0;
-//    wait_us(20);
-//    rest_wifi= 1;   
-// }
+    wait_us(500); // Approximate time value
+    boot_wifi = 0;
+    rest_wifi = 0;
+    wait_us(20);
+    rest_wifi= 1;   
+}
 
-// void pc_callback(void) {
-//     static char ch;
-//     if (pc.readable()) {
-//         static int count = 0;
-//         char ch = pc.getc();
-//         if (ch == '\r') {
-//             wifi.putc('\r');
-//             wifi.putc('\n');
-//         } else {
-//             wifi.putc(ch);   
-//         }
-//     }
-// }
+AnalogIn ch0(P0_23);
 
-// void wifi_callback(void) {
-//     if (wifi.readable())
-//         pc.putc(wifi.getc());
-// }
+int main()
+{
+    Serial pc(USBTX, USBRX);
+    ESP8266Interface net(P2_8, P2_9, true);
 
-// void goto_at_mode(void)
-// {
-//     if (pc.readable())
-//         if (pc.getc() == '#') {
-//             pc.printf("\r\nEntered into AT mode\r\n");
-//             pc.attach(&pc_callback);
-//             wifi.attach(&wifi_callback);
-//             pc.puts("AT\r");
-//             while (1)
-//                 ;
-//         }
-// }
+    pc.baud(115200);
+    pc.printf("\r\nExample network-socket HTTP client\r\n");
+    wifi_init();
+    net.connect("Zilogic Quiz", "zilogic123");
+    const char *ip = net.get_ip_address();
+    const char *mac = net.get_mac_address();
+    pc.printf("IP address is: %s\r\n", ip ? ip : "No IP");
+    pc.printf("MAC address is: %s\r\n", mac ? mac : "No MAC");
 
-// int main()
-// {
-//     pc.attach(goto_at_mode);
-//     pc.baud(115200);
-//     wifi.baud(115200);
-//     parser.debugOn(1);
-//     wifi_init();
+    while (1) {
+        pc.printf("Open socket connection\r\n");
+        TCPSocket socket;
 
-//     pc.printf("\r\nATParser:\r\n");
-//     pc.printf("---------\r\n");
+        int sopen = socket.open(&net);
+        if (sopen < 0)
+            pc.printf("Soc Open ERR: %d\r\n", sopen);
 
-//     bool ret = parser.send("AT+RST")
-//             && parser.recv("OK\r\nready");
-//     pc.printf(">> RST: %d\r\n", ret);
+        int socon = socket.connect("80.243.190.58", 80);
+        if (socon < 0)
+            pc.printf("Soc Conn ERR: %d\r\n", socon);
 
-//     ret = parser.send("AT+CWMODE=%d", 3)
-//             && parser.recv("OK")
-//             && parser.send("AT+CIPMUX=1")
-//             && parser.recv("OK");
-//     pc.printf(">> StartUp: %d\r\n", ret);
-
-//     ret = parser.send("AT+CWDHCP=%d,%d", true?1:0, 1)
-//             && parser.recv("OK");
-//     pc.printf(">> CHCP: %d\r\n", ret);
-
-//     char ap[] = "Zilogic Quiz";
-//     char passPhrase[] = "zilogic123";
-//     ret = parser.send("AT+CWJAP=\"%s\",\"%s\"", ap, passPhrase)
-//             && parser.recv("OK");
-//     pc.printf(">> Conn AP: %d\r\n", ret);
-
-//     char _ip_buffer[30];
-//     parser.send("AT+CIFSR")
-//             && parser.recv("+CIFSR:STAIP,\"%15[^\"]\"", _ip_buffer)
-//             && parser.recv("OK");
-//     pc.printf(">> IP ADD: %s\r\n", _ip_buffer);
-
-//     char _mac_buffer[30];
-//     parser.send("AT+CIFSR")
-//             && parser.recv("+CIFSR:STAMAC,\"%17[^\"]\"", _mac_buffer)
-//             && parser.recv("OK");
-//     pc.printf(">> MAC ADD: %s\r\n", _mac_buffer);
-
-//     for (int i = 1; 1; i++) {
-//         printf("\r\n>> PACK %d\r\n", i);
-//         int id = 0;
-//         char type[] = "TCP";
-//         char addr[] = "80.243.190.58";
-//         int port = 80;
-//         ret = parser.send("AT+CIPSTART=%d,\"%s\",\"%s\",%d", id, type, addr, port)
-//                 && parser.recv("OK");
-//         printf(">> TCP Conn: %d\r\n", ret);
-
-//         char data[255];
-//         sprintf(data, "GET /input/post.json?json={Key:%d}&apikey=1b4e02511d80e5debcc4eea7265f143c\r\nHost: 80.243.190.58\r\n", i);
-//         int amount = strlen(data);
-//         ret = parser.send("AT+CIPSEND=%d,%d", id, amount)
-//                 && parser.recv(">")
-//                 && parser.write((char*)data, (int)amount)
-//                 && parser.recv("CLOSED");
-//         printf(">> Send: %d\r\n", ret);
-//     }
-//     printf("END\r\n\n");
-                
-//     return 0;   
-// }
-
+        // "GET /input/post.json?json={Key:1}&apikey=1b4e02511d80e5debcc4eea7265f143c\r\nHost: 80.243.190.58\r\n"
+        char sbuffer[512] = {0};
+        sprintf(sbuffer,
+                "GET /feed/insert.json?id=149138&time=UNIXTIME&value=%d&apikey=1b4e02511d80e5debcc4eea7265f143c\r\nHost: 80.243.190.58\r\n",
+                ch0.read_u16());
+       
+        int scount = socket.send(sbuffer, strlen(sbuffer) + 1);
+        pc.printf("sent %d [%.*s]\r\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
+   
+        char rbuffer[64];
+        int rcount = socket.recv(rbuffer, sizeof rbuffer);
+        pc.printf("recv %d [%.*s]\r\n", rcount, strstr(rbuffer, "\r\n")-rbuffer, rbuffer);
+   
+        int sclose = socket.close();
+        if (sclose < 0)
+            pc.printf("Soc Close ERR: %d\r\n", sclose);
+    }
+    net.disconnect();
+    pc.printf("Done\r\n");
+}
